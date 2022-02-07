be737b68a0e7b7d1b9c4b4b5033cb889806cba43
Walter <wrkagel@uw.edu>
2022-02-07 11:18:05 -0800

    working problem api functions

--- ProblemIngestion/index.ts
+++ ProblemIngestion/index.ts
@@ -2,7 +2,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { ServiceBusClient } from "@azure/service-bus/";
 import { v4 } from "uuid";
 
-const client = new ServiceBusClient(process.env.paradisevillas_SERVICEBUS)
+const client = new ServiceBusClient(process.env.PROBLEM_QUEUE)
 
 interface Problem{
     id: string
@@ -14,8 +14,8 @@ interface Problem{
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
 
-    const problemRequest:{desc:string} = req.body
-    const problemQueue = client.createSender("problem")
+    const problemRequest:{desc:string} = req.body;
+    const problemQueue = client.createSender("problem");
     const problem: Problem  ={
             id: v4(), 
             desc: problemRequest.desc,
