d8606ac11ed2155a8757ad86327bf6a6228879ea
Walter <wrkagel@uw.edu>
2022-02-11 14:28:20 -0800

    Working file upload endpoint

--- upload-problem-with-photo/index.ts
+++ upload-problem-with-photo/index.ts
@@ -1,13 +1,32 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import busboy from 'busboy'
+import busboy from 'busboy';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+
+    if(!req.query.id || !req.query.ext) {
+        context.res = {
+            body: "Filename or extension not provided",
+            status:400
+        }
+    }
+
     context.log('HTTP trigger function processed a request.');
     const bb = busboy({headers:req.headers});
-    bb.on('file', (name, file, info) => {
-        const {filename, encoding, mimeType} = info;
-        
+    const {id, ext} = req.query;
+    bb.on('file', function(name, file, info) {
+        file.on('data', function(data) {
+            context.bindings.fileBlob = data;
+        });
     });
+    bb.on('finish', function() {
+        console.log('Done parsing form!');
+    
+        context.res = {
+            status: 201,
+            body: `https://wkrevaturestorage.blob.core.windows.net/triple-threat-resort-problems/${id}.${ext}`
+        };
+    });
+    bb.write(req.body);
 
 };
 
