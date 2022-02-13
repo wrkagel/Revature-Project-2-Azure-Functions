86a35b52ece3e46106ee963fb2e19af7a2008a5b
Walter <wrkagel@uw.edu>
2022-02-13 05:45:26 -0800

    Moved update employee into tech-page-relay

--- tech-page-relay/index.ts
+++ tech-page-relay/index.ts
@@ -1,5 +1,6 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import axios from "axios";
+import { Pool } from "pg";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
@@ -40,7 +41,41 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
                 status: 500
             }
         }
-}
+    }   else if(req.method === "PATCH") {
+        context.log('Updating an employee.');
+
+    const pool = new Pool({
+        host:process.env.POSTGRES_HOST,
+        user:process.env.POSTGRES_USER,
+        password:process.env.POSTGRES_PASSWORD,
+        database:"postgres",
+        port:5432,
+        ssl:true
+    });
+
+    const employee = req.body;
+    if(!employee || !employee.password || !employee.id) {
+        context.res = {
+            body:"Missing required information for update.",
+            status:400
+        }
+        return;
+    }
+
+    try {
+        await pool.query("update \"Employee\" set \"password\" = $1 where id = $2", [employee.password, employee.id]);
+        context.res = {
+            body:"Successfully updated."
+        }        
+    } catch (error) {
+        context.res = {
+            body:"Error updating.",
+            status:500
+        }
+    }
+
+    await pool.end();   
+    }
 
 };
 
