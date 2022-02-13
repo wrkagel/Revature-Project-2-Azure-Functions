8f2dcb8929509528df6a60e03346a1bd4ef218f4
Walter <wrkagel@uw.edu>
2022-02-13 15:08:21 -0800

    fixed tab spacing in file.

--- tech-page-relay/index.ts
+++ tech-page-relay/index.ts
@@ -41,40 +41,40 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
                 status: 500
             }
         }
-    }   else if(req.method === "PATCH") {
+    } else if(req.method === "PATCH") {
         context.log('Updating an employee.');
 
-    const pool = new Pool({
-        host:process.env.POSTGRES_HOST,
-        user:process.env.POSTGRES_USER,
-        password:process.env.POSTGRES_PASSWORD,
-        database:"postgres",
-        port:5432,
-        ssl:true
-    });
+        const pool = new Pool({
+            host:process.env.POSTGRES_HOST,
+            user:process.env.POSTGRES_USER,
+            password:process.env.POSTGRES_PASSWORD,
+            database:"postgres",
+            port:5432,
+            ssl:true
+        });
 
-    const employee = req.body;
-    if(!employee || !employee.password || !employee.id) {
-        context.res = {
-            body:"Missing required information for update.",
-            status:400
+        const employee = req.body;
+        if(!employee || !employee.password || !employee.id) {
+            context.res = {
+                body:"Missing required information for update.",
+                status:400
+            }
+            return;
         }
-        return;
-    }
 
-    try {
-        await pool.query("update \"Employee\" set \"password\" = $1 where id = $2", [employee.password, employee.id]);
-        context.res = {
-            body:"Successfully updated."
-        }        
-    } catch (error) {
-        context.res = {
-            body:"Error updating.",
-            status:500
+        try {
+            await pool.query("update \"Employee\" set \"password\" = $1 where id = $2", [employee.password, employee.id]);
+            context.res = {
+                body:"Successfully updated."
+            }        
+        } catch (error) {
+            context.res = {
+                body:"Error updating.",
+                status:500
+            }
         }
-    }
 
-    await pool.end();   
+        await pool.end();   
     }
 
 };
